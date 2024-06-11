ba7ff88cd7fae06fc32281cd76691ac3ef605eae
Hosam <your.email@example.com>
2024-06-12 02:19:22 +0300

    aaaaa

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+interface IProps {}
+
+const ProductCard = ({}: IProps) => {
+  return (
+    <div className="border border-white text-white rounded-md text-sm p-3">
+      <span>ProductCard</span>
+    </div>
+  );
+};
+
+export default ProductCard;
